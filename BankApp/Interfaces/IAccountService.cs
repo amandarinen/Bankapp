a1129e008567475c2dcf898e1ff3f120fd8174c8
Amanda <amanda@leijonborg.se>
2025-10-09 12:51:49 +0200

    New interfaces, services and domain

---
+++ BankApp/Interfaces/IAccountService.cs
@@ -0,0 +1,8 @@
+﻿namespace BankApp.Interfaces
+{
+    public interface IAccountService
+    {
+        IBankAccount CreateAccount(string name, string currency, decimal initialBalance); //account typ? vanligt konto eller sparkonto?
+        List<IBankAccount> GetAccounts();
+    }
+}
---
+++ BankApp/Interfaces/IBankAccount.cs
@@ -0,0 +1,18 @@
+﻿namespace BankApp.Interfaces
+{
+    /// <summary>
+    /// Interface containing the BankAccount methods
+    /// </summary>
+    public interface IBankAccount
+    {
+        Guid Id { get; } 
+        string Name { get; }
+        string Currency { get; }
+        decimal Balance { get; }
+        DateTime LastUpdated { get; }
+        void Withdraw(decimal amount);
+        void Deposit(decimal amount);
+
+
+    }
+}
