23b140b1370a575fb4b573e1644e3e4d30cf9fda
Amanda <amanda@leijonborg.se>
2025-10-13 12:16:48 +0200

    Transaction och ändrat i BankAccount

--- BlazorApp4/Domain/BankAccount.cs
+++ BlazorApp4/Domain/BankAccount.cs
@@ -16,6 +16,8 @@ namespace BlazorApp4.Domain
 
         public DateTime LastUpdated { get; private set; }
 
+        public List<Transaction> Transactions { get; private set; } = new();
+
         public BankAccount(string name, AccountType accountType, CurrencyType currency, decimal initialBalance)
         {
             Name = name;
@@ -25,22 +27,35 @@ namespace BlazorApp4.Domain
             LastUpdated = DateTime.Now;
         }
 
-
-
-
         public void Deposit(decimal amount)
         {
-            
-            if (amount < 0) { throw new NotImplementedException(); }
-
+            if (amount <= 0) throw new ArgumentException("Beloppet måste vara större än 0!");
             Balance += amount;
+            LastUpdated = DateTime.Now;
+
+            Transactions.Add(new Transaction
+            {
+                Type = TransactionType.Deposit,
+                Amount = amount,
+                BalanceAfterTransaction = Balance
+            });
         }
 
         public void Withdraw(decimal amount)
         {
-            if (amount < 0) { throw new NotImplementedException(); }
+            if (amount <= 0) throw new ArgumentException("Beloppet måste vara större än 0!");
 
+            if (Balance < amount) throw new InvalidOperationException("Inte tillräckligt saldo!");
             Balance -= amount;
+            LastUpdated = DateTime.Now;
+
+            Transactions.Add(new Transaction
+            {
+                Type = TransactionType.Withdrawal,
+                Amount = amount,
+                BalanceAfterTransaction = Balance
+            });
         }
     }
 }
+
---
+++ BlazorApp4/Domain/Transaction.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorApp4.Domain
+{
+    public enum TransactionType
+    {
+        Deposit,
+        Withdrawal,
+        Transfer
+    }
+
+    public class Transaction
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime Date { get; set; } = DateTime.Now;
+        public TransactionType Type { get; set; }
+        public decimal Amount { get; set; }
+        public CurrencyType Currency { get; set; }
+        public decimal BalanceAfterTransaction { get; set; }
+        public Guid? FromAccountId { get; set; }
+        public Guid? ToAccountId { get; set; }
+    }
+}
